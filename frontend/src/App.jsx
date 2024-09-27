3f97adf4a7439c82315259627e8bac3a8c97bea1
Syed Hisham Shah <syedhishamshah27@gmail.com>
2024-09-28 01:01:44 +0500

    add layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./index.css";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default Layout;
